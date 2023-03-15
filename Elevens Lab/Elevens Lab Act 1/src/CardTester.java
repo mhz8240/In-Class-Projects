bb00f63e4bd3269e102895141ac7ad69b8a7166e
mhz8240 <michaelzheng119@gmail.com>
2023-03-14 18:02:11 -0700

    Elevens Lab

---
+++ Elevens Lab/Elevens Lab Act 1/src/CardTester.java
@@ -0,0 +1,18 @@
+
+
+/**
+ * This is a class that tests the Card class.
+ */
+
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("Spade", "King", 10);
+		System.out.println(card1.rank());
+	}
+}
